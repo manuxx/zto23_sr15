7bae620850a7d181efcf5be2d5fda0e8a70c9d5f
I-WOObin-I <lubin07000@gmail.com>
2023-11-08 16:47:18 +0100

    zad2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -14,7 +14,10 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore;
+            foreach (var pet in _petsInTheStore)
+            {
+                yield return pet;
+            }
         }
 
         public void Add(Pet newPet)
