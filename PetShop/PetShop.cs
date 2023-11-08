d381f71386462d9a6291f79b59713636bb2d445e
blazej-smorawski <blazej.smorawki@gmail.com>
2023-11-08 16:18:54 +0100

    Zadanie 2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -19,6 +19,15 @@ namespace Training.DomainClasses
 
         public void Add(Pet newPet)
         {
+            foreach (Pet pet in _petsInTheStore)
+            {
+                if (pet.name == newPet.name)
+                {
+                    return;
+                }
+            }
+
+            _petsInTheStore.Add(newPet);
         }
     }
 }
