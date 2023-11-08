3bf6d7095ec991d557dcdae0a675787b91ab8819
Bezimienii <czerminskijakub@gmail.com>
2023-11-08 16:18:16 +0100

    Added Equals in Pet.

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -2,12 +2,43 @@ using System;
 
 namespace Training.DomainClasses
 {
-    public class Pet 
+    public class Pet : IEquatable<Pet>
     {
-        public Sex sex;
+        public bool Equals(Pet other)
+        {
+            if(ReferenceEquals(null, other)) return false;
+            if(ReferenceEquals(this, other)) return true;
+            return this.name == other.name;
+        }
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if(obj.GetType() != typeof(Pet)) return false;
+            return Equals((Pet) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return (name != null ? name.GetHashCode() : 0);
+		}
+
+        public static bool operator ==(Pet left, Pet right)
+        {
+            return Equals(left, right);
+        }
+
+		public static bool operator !=(Pet left, Pet right)
+		{
+			return Equals(left, right);
+		}
+
+		public Sex sex;
         public string name { get; set; }
         public int yearOfBirth { get; set; }
         public float price { get; set; }
         public Species species { get; set; }
-    }
+
+	}
 }
