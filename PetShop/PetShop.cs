ea7be614e803b0ed5ed1795d5d4e6d9bbb1df1d6
manuxx <manus@pg.gda.pl>
2023-11-15 15:43:41 +0100

    wyrefaktoryzowana generyczna extensionMethod OneAtATime

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -14,10 +14,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            foreach (var pet in _petsInTheStore)
-            {
-                yield return pet;
-            }
+            return _petsInTheStore.OneAtATime();
         }
 
         public void Add(Pet newPet)
