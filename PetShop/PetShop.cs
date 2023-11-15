ca8be200b971963a1f1df6b612d2f16781bf517a
Krystian Jandy <krystianj@meritus.pl>
2023-11-15 16:34:50 +0100

    second tests

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -47,6 +47,94 @@ namespace Training.DomainClasses
             ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return ret;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Mouse)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat || pet.species == Species.Dog)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.sex == Sex.Female)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010 && pet.species == Species.Dog)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species != Species.Mouse)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Dog && pet.sex == Sex.Male)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2011 || pet.species == Species.Rabbit)
+                {
+                    yield return pet;
+                }
+            }
+        }
     }
 
     public class ReadOnly<TItem> : IEnumerable<TItem>
