9a8c152299d3d16d5f89bc62a26cb46e0e250828
Bezimienii <czerminskijakub@gmail.com>
2023-11-15 19:59:57 +0100

    Zad3

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Training.DomainClasses
 {
@@ -32,9 +33,14 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCats()
         {
+            return Filter(pet => pet.species == Species.Cat);
+        }
+        
+        public IEnumerable<Pet> Filter(Func<Pet, bool> predicate)
+        {
             foreach (var pet in _petsInTheStore)
             {
-                if (pet.species==Species.Cat)
+                if (predicate.Invoke(pet))
                 {
                     yield return pet;
                 }
@@ -47,7 +53,47 @@ namespace Training.DomainClasses
             ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return ret;
         }
-    }
+
+		public IEnumerable<Pet> AllMice()
+		{
+            return Filter(pet => pet.species == Species.Mouse);
+		}
+
+		public IEnumerable<Pet> AllFemalePets()
+		{
+            return Filter(pet => pet.sex == Sex.Female);
+		}
+
+		public IEnumerable<Pet> AllPetsButNotMice()
+		{
+            return Filter(pet => pet.species != Species.Mouse);
+		}
+
+		public IEnumerable<Pet> AllDogsBornAfter2010()
+		{
+			return Filter(pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog );
+		}
+
+		public IEnumerable<Pet> AllMaleDogs()
+		{
+			return Filter(pet => pet.sex == Sex.Male && pet.species == Species.Dog );
+		}
+
+		public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+		{
+			return Filter(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit  );
+		}
+
+		public IEnumerable<Pet> AllCatsOrDogs()
+		{
+			return Filter(pet => pet.species == Species.Dog || pet.species == Species.Cat  );
+		}
+
+		public IEnumerable<Pet> AllPetsBornAfter2010()
+		{
+			return Filter(pet => pet.yearOfBirth > 2010);
+		}
+	}
 
     public class ReadOnly<TItem> : IEnumerable<TItem>
     {
