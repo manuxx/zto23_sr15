c29944ab896d61dab7104585c07641aa03638552
MicBacon <boczon.michal@gmail.com>
2023-11-15 16:25:40 +0100

    second task

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -28,6 +27,24 @@ namespace Training.DomainClasses
    
             }
             _petsInTheStore.Add(newPet);
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var ret = new List<Pet>(_petsInTheStore);
+            ret.Sort((p1, p2) => p1.name.CompareTo(p2.name));
+            return ret;
         }
     }
 
