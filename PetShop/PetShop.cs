3f9e523c95efd1d859b091e14f3d18b38bcf9b72
Piotr <piotrkopecki01@gmail.com>
2023-11-15 16:33:35 +0100

    kolejny etap

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
+
 namespace Training.DomainClasses
 {
     public class PetShop
@@ -16,7 +17,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return new ReadOnly(_petsInTheStore);
+            return new ReadOnly<Pet>(_petsInTheStore);
         }
 
         private Boolean IsAlredyAdded(string petName)
@@ -39,9 +40,138 @@ namespace Training.DomainClasses
                 _petsInTheStore.Add(newPet);
             }
         }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species.Equals(Species.Cat))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var result = new List<Pet>(_petsInTheStore);
+            result.Sort ((p1, p2) => p1.name.CompareTo(p2.name));
+            return result;
+        }
+
+        public IEnumerable<Pet> AllOfThisType(Species type)
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species.Equals(type))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return AllOfThisType(Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.sex == Sex.Female)
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species.Equals(Species.Cat) || pet.species.Equals(Species.Dog))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (!pet.species.Equals(Species.Mouse))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010)
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010 && pet.species.Equals(Species.Dog))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.sex == Sex.Male && pet.species.Equals(Species.Dog))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            IList<Pet> _petsInTheStore2 = new List<Pet>();
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010 || pet.species.Equals(Species.Rabbit))
+                {
+                    _petsInTheStore2.Add(pet);
+                }
+            }
+            return _petsInTheStore2;
+        }
     }
 
-    public class ReadOnly : IEnumerable<Pet>
+    public class ReadOnly<TItem> : IEnumerable<Pet>
     {
         private readonly IEnumerable<Pet> _pets;
 
@@ -60,4 +190,5 @@ namespace Training.DomainClasses
             return GetEnumerator();
         }
     }
+
 }
--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -113,5 +113,162 @@ namespace Training.Specificaton
         private static Exception exception;
         It invalid_cast_exception_should_be_thrown = () => exception.ShouldBeOfExactType<InvalidCastException>();
     }
+    public class concern_with_pets_for_sorting_and_filtering : pet_shop_concern
+    {
+        private Establish c = () =>
+        {
+            mouse_Dixie = new Pet
+            {
+                name = "Dixie",
+                species = Species.Mouse,
+                price = 10,
+                sex = Sex.Female,
+                yearOfBirth = 2011
+            };
+            mouse_Jerry = new Pet
+            {
+                name = "Jerry",
+                species = Species.Mouse,
+                price = 5,
+                sex = Sex.Male,
+                yearOfBirth = 2012
+            };
+
+            cat_Tom = new Pet
+            {
+                name = "Tom",
+                species = Species.Cat,
+                price = 30,
+                sex = Sex.Male,
+                yearOfBirth = 2010
+            };
+            cat_Jinx = new Pet
+            {
+                name = "Jinx",
+                species = Species.Cat,
+                price = 40,
+                sex = Sex.Male,
+                yearOfBirth = 2009
+            };
+            rabbit_Fluffy = new Pet
+            {
+                name = "Fluffy",
+                species = Species.Rabbit,
+                price = 35,
+                sex = Sex.Male,
+                yearOfBirth = 2011
+            };
+            dog_Huckelberry = new Pet
+            {
+                name = "Huckelberry",
+                species = Species.Dog,
+                price = 80,
+                sex = Sex.Male,
+                yearOfBirth = 2008
+            };
+            dog_Lassie = new Pet
+            {
+                name = "Lassie",
+                species = Species.Dog,
+                price = 150,
+                sex = Sex.Female,
+                yearOfBirth = 2007
+            };
+            dog_Pluto = new Pet
+            {
+                name = "Pluto",
+                species = Species.Dog,
+                price = 100,
+                sex = Sex.Male,
+                yearOfBirth = 2011
+            };
+            pet_initial_content.AddManyItems(cat_Tom,
+                                             cat_Jinx,
+                                             dog_Huckelberry,
+                                             dog_Lassie,
+                                             dog_Pluto,
+                                             rabbit_Fluffy,
+                                             mouse_Dixie,
+                                             mouse_Jerry);
+        };
+
+        protected static Pet mouse_Dixie;
+        protected static Pet mouse_Jerry;
+        protected static Pet rabbit_Fluffy;
+        protected static Pet cat_Jinx;
+        protected static Pet cat_Tom;
+        protected static Pet dog_Huckelberry;
+        protected static Pet dog_Lassie;
+        protected static Pet dog_Pluto;
+    }
+
+    public class when_searching_for_pets : concern_with_pets_for_sorting_and_filtering
+    {
+        private It should_be_able_to_find_all_cats = () =>
+        {
+            var foundPets = subject.AllCats();
+            foundPets.ShouldContainOnly(cat_Tom, cat_Jinx);
+        };
+        private It should_be_able_to_find_all_mice = () =>
+        {
+            var foundPets = subject.AllMice();
+            foundPets.ShouldContainOnly(mouse_Dixie, mouse_Jerry);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_female_pets = () =>
+        {
+            var foundPets = subject.AllFemalePets();
+            foundPets.ShouldContainOnly(dog_Lassie, mouse_Dixie);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_cats_or_dogs = () =>
+        {
+            var foundPets = subject.AllCatsOrDogs();
+            foundPets.ShouldContainOnly(cat_Tom, cat_Jinx, dog_Huckelberry, dog_Lassie, dog_Pluto);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_pets_but_not_mice = () =>
+        {
+            var foundPets = subject.AllPetsButNotMice();
+            foundPets.ShouldContainOnly(cat_Tom, cat_Jinx, dog_Huckelberry, dog_Lassie, dog_Pluto, rabbit_Fluffy);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_pets_born_after_2010 = () =>
+        {
+            var foundPets = subject.AllPetsBornAfter2010();
+            foundPets.ShouldContainOnly(dog_Pluto, rabbit_Fluffy, mouse_Dixie, mouse_Jerry);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_young_dogs = () =>
+        {
+            var foundPets = subject.AllDogsBornAfter2010();
+            foundPets.ShouldContainOnly(dog_Pluto);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_male_dogs = () =>
+        {
+            var foundPets = subject.AllMaleDogs();
+            foundPets.ShouldContainOnly(dog_Huckelberry, dog_Pluto);
+        };
+        [Ignore("Will be implemented next")]
+        private It should_be_able_to_find_all_young_pets_or_rabbits = () =>
+        {
+            var foundPets = subject.AllPetsBornAfter2011OrRabbits();
+            foundPets.ShouldContainOnly(mouse_Jerry, rabbit_Fluffy);
+        };
+
+    }
+
+    class when_sorting_pets : concern_with_pets_for_sorting_and_filtering
+    {
+        It should_be_able_to_sort_by_name_ascending = () =>
+        {
+            var result = subject.AllPetsSortedByName();
+
+            result.ShouldContainOnlyInOrder(mouse_Dixie, rabbit_Fluffy, dog_Huckelberry, mouse_Jerry, cat_Jinx,
+                dog_Lassie,
+                dog_Pluto, cat_Tom);
+        };
+    };
 
 }
