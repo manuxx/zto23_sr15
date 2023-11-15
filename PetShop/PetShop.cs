458a9fa7a556589474b022673b1d5457e89326d5
MicBacon <boczon.michal@gmail.com>
2023-11-15 19:01:24 +0100

    first commit

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -47,6 +47,103 @@ namespace Training.DomainClasses
             ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return ret;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return Filter(_petsInTheStore);
+        }
+
+        private IEnumerable<Pet> Filter(IEnumerable<Pet> pets)
+        {
+            foreach (var pet in pets)
+            {
+                if (pet.species == Species.Mouse)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.sex == Sex.Female)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat ||
+                    pet.species == Species.Dog)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species != Species.Mouse)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2010 &&
+                    pet.species == Species.Dog)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.sex == Sex.Male &&
+                    pet.species == Species.Dog)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.yearOfBirth > 2011 ||
+                    pet.species == Species.Rabbit)
+                {
+                    yield return pet;
+                }
+            }
+        }
     }
 
     public class ReadOnly<TItem> : IEnumerable<TItem>
