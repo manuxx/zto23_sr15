047d6c1f657b24d45b57924b19a59b168c2775b4
I-WOObin-I <lubin07000@gmail.com>
2023-11-15 15:43:41 +0100

    generic method created

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -14,10 +14,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            foreach (var pet in _petsInTheStore)
-            {
-                yield return pet;
-            }
+            return EnumeratorUtil.YieldAll(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
