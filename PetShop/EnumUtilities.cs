2ec2ab5dedf216c048ecfd2b4dee32c02d5ce0ab
migowskiD <dawid.migowski@gmail.com>
2023-11-22 15:33:44 +0100

    first commit

--- PetShop/EnumUtilities.cs
+++ PetShop/EnumUtilities.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Training.DomainClasses;
 
@@ -8,6 +9,17 @@ public static class EnumUtilities
         foreach (var item in items)
         {
             yield return item;
+        }
+    }
+
+    public static IEnumerable<TItem> Filter<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> condition)
+    {
+        foreach (var item in items)
+        {
+            if (condition(item))
+            {
+                yield return item;
+            }
         }
     }
 }
