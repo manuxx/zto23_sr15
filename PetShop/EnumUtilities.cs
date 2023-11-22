2471b72d16bf74a81c7ddf90ce8ca4a208b4948a
manuxx <manus@pg.gda.pl>
2023-11-22 16:11:03 +0100

    zadanie anonymous criteria

--- PetShop/EnumUtilities.cs
+++ PetShop/EnumUtilities.cs
@@ -14,12 +14,35 @@ public static class EnumUtilities
 
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
     {
+        Criteria<TItem> adapter = new AnonymousCriteria<TItem>(condition);
+        return items.ThatSatisfy(adapter);
+    }
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
+    {
         foreach (var item in items)
         {
-            if (condition(item))
+            if (criteria.IsSatisfiedBy(item))
             {
                 yield return item;
             }
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface Criteria<T>
+{
+    bool IsSatisfiedBy(T item);
 }
