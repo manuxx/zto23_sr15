28d1271432ea3a7febe6914f28f037aa7ea3a777
MicBacon <boczon.michal@gmail.com>
2023-11-22 16:17:50 +0100

    criteria

--- PetShop/EnumUtilities.cs
+++ PetShop/EnumUtilities.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Training.DomainClasses;
+using static EnumUtilities;
 
 public static class EnumUtilities
 {
@@ -14,12 +18,37 @@ public static class EnumUtilities
 
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
     {
+        Criteria<TItem> adapter = new AnonymousCriteria<TItem>(condition);
+        return items.ThatSatisfy(adapter);
+    }
+
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
+    {
         foreach (var item in items)
         {
-            if (condition(item))
+            if (criteria.IsSatisfiedBy(item))
             {
                 yield return item;
             }
         }
+    }
+
+    public interface Criteria<T>
+    {
+        public bool IsSatisfiedBy(T item);
+    }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    private Predicate<T> _condition;
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return _condition(item);
     }
 }
