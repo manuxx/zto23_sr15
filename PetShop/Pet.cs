dc4a0ec5329fd99a50fda252ea85842a38e76b75
Krystian Jandy <krystianj@meritus.pl>
2023-11-22 15:56:38 +0100

    move to Pet type

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -9,5 +9,25 @@ namespace Training.DomainClasses
         public int yearOfBirth { get; set; }
         public float price { get; set; }
         public Species species { get; set; }
+
+        public static Predicate<Pet> IsSpeciesOf(Species species)
+        {
+            return pet => pet.species == species;
+        }
+
+        public static Predicate<Pet> IsSexOf(Sex female)
+        {
+            return pet => pet.sex == female;
+        }
+
+        public static Predicate<Pet> IsNotSpeciesOf(Species species)
+        {
+            return pet => pet.species != species;
+        }
+
+        public static Predicate<Pet> IsBornAfter(int year)
+        {
+            return pet => pet.yearOfBirth > year;
+        }
     }
 }
