1bbb161001496ad8599cfc1c534ea75b78ae9a03
manuxx <manus@pg.gda.pl>
2023-11-29 16:35:05 +0100

    dziala ale to mega slabe jest

--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -208,7 +208,8 @@ namespace Training.Specificaton
     {
         private It should_be_able_to_find_all_cats = () =>
         {
-            var foundPets = subject.AllCats();
+            var criteria = Where<Pet>.HasAn(p => p.species).EqualTo(Species.Cat);
+            var foundPets = subject.AllPets().ThatSatisfy(criteria);
             foundPets.ShouldContainOnly(cat_Tom, cat_Jinx);
         };
         private It should_be_able_to_find_all_mice = () =>
@@ -235,7 +236,8 @@ namespace Training.Specificaton
         };
         private It should_be_able_to_find_all_pets_born_after_2010 = () =>
         {
-            var foundPets = subject.AllPetsBornAfter2010();
+            var criteria = Where<Pet>.HasComparable(p => p.yearOfBirth).GreaterThan(2010);
+            var foundPets = subject.AllPets().ThatSatisfy(criteria);
             foundPets.ShouldContainOnly(dog_Pluto, rabbit_Fluffy, mouse_Dixie, mouse_Jerry);
         };
         private It should_be_able_to_find_all_young_dogs = () =>
@@ -262,6 +264,11 @@ namespace Training.Specificaton
         {
             return new CriteriaBuilder<TItem,TProperty>(propertySelector);
         }
+        public static ComparableCriteriaBuilder<TItem, TProperty> HasComparable<TProperty>(Func<TItem, TProperty> propertySelector)
+            where TProperty : IComparable<TProperty>
+        {
+            return new ComparableCriteriaBuilder<TItem, TProperty>(propertySelector);
+        }
     }
 
     internal class CriteriaBuilder<TItem,TProperty>
@@ -277,8 +284,28 @@ namespace Training.Specificaton
         {
             return new AnonymousCriteria<TItem>(i=>_propertySelector(i).Equals(value));
         }
+
     }
 
+    internal class ComparableCriteriaBuilder<TItem, TProperty> where TProperty : IComparable<TProperty>
+    {
+        private readonly Func<TItem, TProperty> _propertySelector;
+
+        public ComparableCriteriaBuilder(Func<TItem, TProperty> propertySelector)
+        {
+            _propertySelector = propertySelector;
+        }
+
+        public Criteria<TItem> EqualTo(TProperty value)
+        {
+            return new AnonymousCriteria<TItem>(i => _propertySelector(i).Equals(value));
+        }
+
+        public Criteria<TItem> GreaterThan(TProperty v)
+        {
+            return new AnonymousCriteria<TItem>(i => _propertySelector(i).CompareTo(v) > 0);
+        }
+    }
     class when_sorting_pets : concern_with_pets_for_sorting_and_filtering
     {
         It should_be_able_to_sort_by_name_ascending = () =>
