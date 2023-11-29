e5a915b86000e7f99fd603b17d30d061b4d6d0c4
Varker00 <lukaszhein00@gmail.com>
2023-11-29 16:21:16 +0100

    2

---
+++ PetShop/Where.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Training.DomainClasses;
+
+
+    public static class Where<TItem>
+    {
+        public static CriteriaBuilder<TItem, TProperty> HasAn<TProperty>(Func<TItem, TProperty> propertySelector)
+        {
+            
+            return new CriteriaBuilder<TItem, TProperty>(propertySelector);
+        }
+    }
+
+    public class CriteriaBuilder<TItem, TProperty>
+    {
+        private Func<TItem, TProperty> _propertySelector;
+
+        public CriteriaBuilder(Func<TItem, TProperty> propertySelector)
+        {
+            _propertySelector = propertySelector;
+        }
+
+        public Criteria<TItem> EqualTo(Species species)
+        {
+            return new AnonymousCriteria<TItem>(item => _propertySelector(item).Equals(species));
+        }
+    }
+
