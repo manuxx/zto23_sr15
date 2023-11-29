5fd7c19c5478197eb66762a9e892a8c127fd610b
manuxx <manus@pg.gda.pl>
2023-11-29 11:48:03 +0100

    Zmiana kryteriow na zagniezdzone

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -29,50 +29,51 @@ namespace Training.DomainClasses
         {
             return new BornAfterCriteria(year);
         }
+        public class SexCriteria : Criteria<Pet>
+        {
+            private readonly Sex _sex;
+
+            public SexCriteria(Sex sex)
+            {
+                _sex = sex;
+            }
+
+            public bool IsSatisfiedBy(Pet pet)
+            {
+                return pet.sex == _sex;
+            }
+        }
+
+        public class BornAfterCriteria : Criteria<Pet>
+        {
+            private readonly int _year;
+
+            public BornAfterCriteria(int year)
+            {
+                _year = year;
+            }
+
+            public bool IsSatisfiedBy(Pet pet)
+            {
+                return pet.yearOfBirth > _year;
+            }
+        }
+
+        public class SpeciesCriteria : Criteria<Pet>
+        {
+            private readonly Species _species;
+
+            public SpeciesCriteria(Species species)
+            {
+                _species = species;
+            }
+
+            public bool IsSatisfiedBy(Pet pet)
+            {
+                return pet.species == _species;
+            }
+        }
     }
 
-    public class SexCriteria : Criteria<Pet>
-    {
-        private readonly Sex _sex;
-
-        public SexCriteria(Sex sex)
-        {
-            _sex = sex;
-        }
-
-        public bool IsSatisfiedBy(Pet pet)
-        {
-            return pet.sex == _sex;
-        }
-    }
-
-    public class BornAfterCriteria : Criteria<Pet>
-    {
-        private readonly int _year;
-
-        public BornAfterCriteria(int year)
-        {
-            _year = year;
-        }
-
-        public bool IsSatisfiedBy(Pet pet)
-        {
-            return pet.yearOfBirth > _year;
-        }
-    }
-
-    public class SpeciesCriteria : Criteria<Pet>
-    {
-        private readonly Species _species;
-
-        public SpeciesCriteria(Species species)
-        {
-            _species = species;
-        }
-
-        public bool IsSatisfiedBy(Pet pet)
-        {
-            return pet.species == _species;
-        }
-    }
+    
 }
