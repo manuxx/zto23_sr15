06feece242ab3c96fee2f5e8e7c65e2b79fb37f9
Micha <michaqu11@wp.pl>
2023-11-29 15:58:55 +0100

    zad3

--- PetShop/EnumUtilities.cs
+++ PetShop/EnumUtilities.cs
@@ -11,11 +11,6 @@ public static class EnumUtilities
             yield return item;
         }
     }
-
-    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.ThatSatisfy(new AnonymousCriteria<TItem>(condition));
-    }
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
     {
         foreach (var item in items)
