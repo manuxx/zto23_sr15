f85fb73bc57d9509eeff051dad43ade6a15c7059
Micha <michaqu11@wp.pl>
2023-11-29 15:26:47 +0100

    zad1-dekorator

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -15,9 +15,9 @@ namespace Training.DomainClasses
             return new SexCriteria(Sex.Female);
         }
 
-        public static Predicate<Pet> IsNotASpeciesOf(Species species)
+        public static Criteria<Pet> IsNotASpeciesOf(Species species)
         {
-            return (pet => pet.species != species);
+            return new Negation<Pet>(Pet.IsASpeciesOf(species));
         }
 
         public static Criteria<Pet> IsASpeciesOf(Species species)
@@ -75,5 +75,18 @@ namespace Training.DomainClasses
         }
     }
 
-    
+    public class Negation<TItem> : Criteria<TItem>
+    {
+
+        private readonly Criteria<TItem> _innerCteria;
+        public Negation(Criteria<TItem> innerCteria)
+        {
+            _innerCteria = innerCteria;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return !_innerCteria.IsSatisfiedBy(item);
+        }
+    }
 }
